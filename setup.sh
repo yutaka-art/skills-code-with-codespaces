18c8918075be659cfe31675eee432127bbf09d11
yutaka-art <53134237+yutaka-art@users.noreply.github.com>
2025-05-18 12:36:39 +0000

    修正

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# slコマンドのインストール
 sudo apt-get update
-sudo apt-get install sl
-echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+sudo apt-get install -y sl
+
+# /usr/gamesがPATHに含まれていなければ~/.bashrcに追記
+if ! grep -q '/usr/games' ~/.bashrc; then
+  echo 'export PATH="$PATH:/usr/games"' >> ~/.bashrc
+fi
+# 現在のシェルにも即時反映
+export PATH="$PATH:/usr/games"
